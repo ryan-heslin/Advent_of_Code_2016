61d1fbdf8fd76f7353ff20e1381be7e85c1a2762
ryan-heslin <rwheslin@gmail.com>
2023-06-02 09:19:52 -0400

    More progress

--- solutions/day11b.py
+++ solutions/day11b.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
+from itertools import chain
+from itertools import combinations
 from math import inf
 from queue import PriorityQueue
+from string import punctuation
 
 ELEMENTS = {
     "hydrogen",
@@ -136,36 +139,36 @@ def clamp(low, high, x):
 # Must bring at least one item
 
 
+def floor_valid(microchips, generators):
+    return not (generators and microchips - generators)
+
+
 def validate(
-    target,
-    index,
-    component,
-    floors,
-    player_floor,
-    target_generators,
-    target_microchips,
-    player_generators,
     player_microchips,
+    player_generators,
+    target_microchips,
+    target_generators,
+    taken_microchips,
+    taken_generators,
 ):
-    if component == 0 and (not target_generators or index in target_microchips):
-        return (target, floors[index][1])
-    # If moving single generator:
-    # Invalid if same microchip type on current floor and other generator types
-    # Invalid if other microchip type on target without own microchip on target
-    elif component == 1 and not (
-        (player_generators - {index} and index in player_microchips)
-        or (target_microchips and index not in target_microchips)
-    ):
-        return (floors[index][0], target)
+    new_player_microchips = player_microchips - taken_microchips
+    new_player_generators = player_generators - taken_generators
+    if not floor_valid(new_player_microchips, new_player_generators):
+        return False
+
+    new_target_microchips = target_microchips | taken_microchips
+    new_target_generators = target_generators | taken_generators
+    return floor_valid(new_target_microchips, new_target_generators)
 
 
 def find_valid_moves(state):
-    # breakpoint()
+    # if state == (1, ((0, 2), (1, 1))):
+    #     breakpoint()
     player_floor, floors = state
     targets = TARGETS[player_floor]
     result = set()
 
-    for target in targets:
+    for target_floor in targets:
         # Unpaired generator on floor above
         # player_microchips = set()
         player_generators = set()
@@ -182,72 +185,83 @@ def find_valid_moves(state):
 
         for i, pair in enumerate(floors):
             # microchips
-            if pair[0] == target:
+            if pair[0] == target_floor:
                 target_microchips.add(i)
             elif pair[0] == player_floor:
                 movable.add((i, 0))
-                # player_microchips.add(i)
+                player_microchips.add(i)
             # generators
-            if pair[1] == target:
+            if pair[1] == target_floor:
                 target_generators.add(i)
             elif pair[1] == player_floor:
                 movable.add((i, 1))
                 player_generators.add(i)
 
-        pairs = set()
-        # breakpoint()
-        for option in movable:
-            index, component = option
-            # Chip
-            replacement = validate(
-                target,
-                index,
-                component,
-                floors,
-                player_floor,
-                target_generators,
-                target_microchips,
-                player_generators,
-                player_microchips,
-            )
-            if component == 0:
-                updated_target_microchips = target_microchips | {index}
-                updated_target_generators = target_generators
-                updated_player_generators = player_generators
-                updated_target_microchips = player_microchips - {index}
-            else:
-                updated_target_microchips = target_microchips
-                updated_player_generators = player_generators - {index}
-                updated_target_generators = target_generators | {index}
-                updated_player_microchips = player_microchips
-            if replacement:
-                new = (target, floors[:index] + (replacement,) + floors[index + 1 :])
-                result.add(new)
-                # Check possible combinations
-                for second in movable:
-                    if second != option and (
-                        pair := tuple(sorted((option, second))) not in pairs
-                    ):
-                        double_replacement = validate(
-                            target,
-                            *second,
-                            new[1],
-                            player_floor,
-                            updated_target_generators,
-                            updated_target_microchips,
-                            updated_player_generators,
-                            updated_target_generators,
-                        )
-                        pairs.add(pair)
-                        if double_replacement:
-                            result.add(
-                                (
-                                    target,
-                                    floors[:index]
-                                    + (double_replacement,)
-                                    + floors[index + 1 :],
-                                )
-                            )
+        choices = chain(movable, combinations(movable, r=2))
+        common_args = {
+            "player_microchips": player_microchips,
+            "player_generators": player_generators,
+            "target_microchips": target_microchips,
+            "target_generators": target_generators,
+        }
+        for choice in choices:
+            match choice:
+                # Single microchip
+                case (i, 0):
+                    var_args = {"taken_microchips": {i}, "taken_generators": set()}
+                    replacements = {i: (target_floor, floors[i][1])}
+                case (i, 1):
+                    var_args = {"taken_microchips": set(), "taken_generators": {i}}
+                    replacements = {i: (floors[i][0], target_floor)}
+
+                    # Two microchips
+                case ((i1, 0), (i2, 0)):
+                    var_args = {"taken_microchips": {i1, i2}, "taken_generators": set()}
+                    replacements = {
+                        i1: (target_floor, floors[i1][1]),
+                        i2: (target_floor, floors[i2][1]),
+                    }
+
+                # Two generators
+                case ((i1, 1), (i2, 1)):
+                    var_args = {"taken_microchips": set(), "taken_generators": {i1, i2}}
+                    replacements = {
+                        i1: (floors[i1][0], target_floor),
+                        i2: (floors[i2][0], target_floor),
+                    }
+                # microchip-generator
+                case ((i1, 0), (i2, 1)):
+                    var_args = {"taken_microchips": {i1}, "taken_generators": {i2}}
+                    if i1 == i2:
+                        replacements = {i1: (target_floor, target_floor)}
+                    else:
+                        replacements = {
+                            i1: (target_floor, floors[i1][1]),
+                            i2: (floors[i2][0], target_floor),
+                        }
+                # Generator-microchip
+                case ((i1, 1), (i2, 0)):
+                    var_args = {"taken_microchips": {i2}, "taken_generators": {i1}}
+                    if i1 == i2:
+                        replacements = {i1: (target_floor, target_floor)}
+                    else:
+                        replacements = {
+                            i1: (floors[i1][0], target_floor),
+                            i2: (
+                                target_floor,
+                                floors[i2][1],
+                            ),
+                        }
+                case _:
+                    raise ValueError
+
+            if validate(**common_args, **var_args):
+                copy = list(floors)
+                for position, replacement in replacements.items():
+                    copy[position] = replacement
+                result.add((target_floor, tuple(sorted(copy))))
+
+    print(state)
     print(result)
     print("\n")
     return result
@@ -268,8 +282,8 @@ def A_star(start, goal):
         # print(current_state)
         # If h never overestimates, as it must for A* to
         # be correct, this will only trigger if the path is suboptimal
-        if h_score + g_score[current_state] >= g_score[goal] or current_state == goal:
-            continue
+        # if h_score + g_score[current_state] >= g_score[goal] or current_state == goal:
+        #     continue
 
         neighbors = find_valid_moves(current_state)
         candidate_g_score = g_score[current_state] + 1
@@ -287,31 +301,33 @@ def A_star(start, goal):
 
 # Bugged - overestimates even when heuristic ignored
 def h(state):
-    return 0
-    # return sum(map(sum, state[1])) // 2
+    # return 0
+    return sum(map(sum, state[1])) // 2
 
 
 # (microchip, generator) pairs
 def parse(lines):
     found = {}
     result = []
+    remover = str.maketrans("", "", punctuation)
     for floor, line in enumerate(lines):
+        # breakpoint()
         words = line.split(" ")
         while words:
-            current = words.pop().split("-")[0].replace(",", "")
+            current = words.pop(0).split("-")[0].replace(",", "")
             if current in ELEMENTS:
-                component = words[0]
+                component = words[0].translate(remover)
                 # Paired component found?
                 if current in found:
                     position = found[current]
                     chip, gen = result[position]
-                    result[position] = (
-                        (chip, floor) if component == "generator" else (floor, gen)
-                    )
+                    result[position] = (chip, floor) if gen is None else (floor, gen)
                 else:
                     found[current] = len(result)
+                    # breakpoint()
                     new = (floor, None) if component == "microchip" else (None, floor)
                     result.append(new)
+                print(result)
 
     return tuple(sorted(result))
 
